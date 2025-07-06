92bebf2371f41c38df0ecf587da7d4ce550a6d95
valmuthu <115936763+valmuthu@users.noreply.github.com>
2025-07-06 00:05:37 -0400

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models');
 
 // Enhanced authentication middleware with better error handling
 const authenticateToken = async (req, res, next) => {
